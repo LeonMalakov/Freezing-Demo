c9c858bcc273d8ec471ca2b0e9148139258bc20f
Leon Malakov <leon-malakov@mail.ru>
2021-07-26 14:18:57 +0300

    Trees snow

---
+++ Assets/Game/Scripts/Common/CoroutineAnimation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace WGame
+{
+    public static class CoroutineAnimation
+    {
+        public static IEnumerator Scale(Transform target, Vector3 from, Vector3 to, Func<float, float> ease, float duration)
+        {
+            float t = 0;
+            while (t < 1)
+            {
+                t += Time.deltaTime / duration;
+                target.localScale = Vector3.Lerp(from, to, ease(t));
+                yield return null;
+            }
+        }
+
+        public static IEnumerator Rotate(Transform target, Quaternion from, Quaternion to, Func<float, float> ease, float duration)
+        {
+            float t = 0;
+            while (t < 1)
+            {
+                t += Time.deltaTime / duration;
+                target.localRotation = Quaternion.Lerp(from, to, ease(t));
+                yield return null;
+            }
+        }
+    }
+}
--- Assets/Game/Scripts/Tree/TreeView.cs
+++ Assets/Game/Scripts/Tree/TreeView.cs
@@ -6,49 +6,60 @@ namespace WGame
 {
     public class TreeView : MonoBehaviour
     {
-        [SerializeField] private GameObject _stem;
+        private const float DyingAnimationDuration = 1.5f;
+        private const float SnowAppearingAnimationDuration = 3f;
+        private const float RecoverAnimationDuration = 3f;
+        [SerializeField] private MeshRenderer _stem;
+        [SerializeField] private MeshRenderer _stemSnow;
+
+        private void Awake()
+        {
+            _stemSnow.enabled = false;
+        }
 
         public void Die(Action callback)
         {
-            StartCoroutine(DyingAnimation(1.5f, callback));
+            StartCoroutine(DyingAnimation(DyingAnimationDuration,
+                callback: () =>
+            {
+                callback?.Invoke();
+                StartCoroutine(SnowAppearingAnimation(SnowAppearingAnimationDuration));
+            }));
         }
 
         public void Recover(Action callback)
         {
-            StartCoroutine(RecoverAnimation(3f, callback));
+            StartCoroutine(RecoverAnimation(RecoverAnimationDuration, callback));
         }
 
         private IEnumerator DyingAnimation(float duration, Action callback)
         {
-            Transform stem = _stem.transform;
+            Transform stemTransform = _stem.transform;
 
-            float t = 0;
-            Quaternion startRotation = stem.localRotation;
+            Quaternion startRotation = stemTransform.localRotation;
             Quaternion targetRotation = Quaternion.Euler(90, 0, 0);
-            while(t < 1)
-            {
-                t += Time.deltaTime / duration;
-                stem.localRotation = Quaternion.Lerp(startRotation, targetRotation, Ease.InCubic(t));
-                yield return null;
-            }
+            yield return StartCoroutine(CoroutineAnimation.Rotate(stemTransform, startRotation, targetRotation, Ease.InCubic, duration));
 
-            _stem.SetActive(false);
+            _stem.enabled = false;
+            stemTransform.localRotation = Quaternion.identity;
             callback?.Invoke();
+        }
+
+        private IEnumerator SnowAppearingAnimation(float duration)
+        {
+            _stemSnow.enabled = true;
+            yield return StartCoroutine(CoroutineAnimation.Scale(_stemSnow.transform, Vector3.zero, Vector3.one, Ease.OutQuad, duration));
         }
 
         private IEnumerator RecoverAnimation(float duration, Action callback)
         {
-            Transform stem = _stem.transform;
-            _stem.SetActive(true);
+            Transform stemTransform = _stem.transform;
+            _stem.enabled = true;
 
-            float t = 0;
-            stem.localRotation = Quaternion.identity;
-            while (t < 1)
-            {
-                t += Time.deltaTime / duration;
-                stem.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, Ease.OutQuad(t));
-                yield return null;
-            }
+            StartCoroutine(CoroutineAnimation.Scale(_stemSnow.transform, Vector3.one, Vector3.zero, Ease.InQuad, duration));
+            yield return StartCoroutine(CoroutineAnimation.Scale(stemTransform, Vector3.zero, Vector3.one, Ease.OutQuad, duration));
+
+            _stemSnow.enabled = false;
 
             callback?.Invoke();
         }
